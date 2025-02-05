ede936c647f9d7c703dba7bd66ff16e7f67a0e4c
kevinzhou <kevinzhou716@gmail.com>
2025-02-05 11:21:40 -0800

    Finished lab2

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,72 @@
+import "./index.css";
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus varius
+        vestibulum ultricies. Vestibulum in orci nec metus vehicula sagittis
+        quis quis quam. Aliquam ut leo diam. Curabitur tempus enim sapien, ut
+        tempus mauris ultrices quis. Suspendisse potenti. Vestibulum pulvinar
+        erat eu tincidunt iaculis.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Sed nec enim sed neque dictum dapibus. Etiam malesuada purus nunc, ac
+        volutpat turpis tempus quis. Integer vel neque vehicula, dapibus enim
+        tincidunt, luctus nisi. Duis ante risus, efficitur sit amet ante id,
+        accumsan efficitur nisi. Cras vitae consectetur risus, vitae egestas
+        quam. Etiam viverra consequat massa, at blandit neque mollis sed. Nulla
+        finibus metus non ipsum venenatis ullamcorper.
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Nunc libero massa, venenatis porta turpis tincidunt, rutrum ornare ex.
+        In tincidunt mattis nisl. Vivamus tincidunt, lectus non convallis
+        dignissim, augue mi pharetra nibh, vel aliquet lorem ipsum a justo. Duis
+        a hendrerit lectus. Proin in molestie justo. Proin finibus ultrices
+        pharetra. Maecenas et enim nulla. Morbi pretium risus lacus, ut
+        vulputate est efficitur eget. Vivamus tristique, lacus sed pellentesque
+        feugiat, purus nisl imperdiet nunc, a tincidunt nunc lectus sit amet
+        justo.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Donec tempor dolor nec pretium venenatis. Aliquam auctor risus nec lacus
+        sodales mollis. Etiam mauris felis, dictum in sapien ac, bibendum
+        ullamcorper enim. Sed nibh felis, placerat vel metus id, elementum
+        placerat leo. Aliquam erat volutpat. Nam interdum eu ante vel sagittis.
+        Suspendisse vulputate feugiat lectus viverra hendrerit. Curabitur lectus
+        erat, finibus sit amet lacinia ut, fringilla sit amet dolor. Cras
+        elementum sagittis leo non tempus. Donec vel sapien eu dolor pulvinar
+        consequat. Vestibulum porta a sem et aliquam. Vestibulum id hendrerit
+        ipsum. Duis finibus ipsum nec commodo congue. Etiam vitae lacus eget
+        orci condimentum suscipit. Fusce condimentum eget enim quis hendrerit.
+        <div className="wd-float-done"></div>
+      </div>
+      <div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+          Yellow{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+          Blue{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+          Red{" "}
+        </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <div className="wd-float-done"></div>
+      </div>
+    </div>
+  );
+}
